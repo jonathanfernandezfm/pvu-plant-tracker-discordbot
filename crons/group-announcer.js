efa19f4ac6f55859836bd24fea829da386180759
Jonathan FM <jonathanfernandezfm@gmail.com>
2021-08-29 21:50:49 +0200

    Fixed horario

--- crons/group-announcer.js
+++ crons/group-announcer.js
@@ -1,16 +1,17 @@
 const moment = require('moment');
 
-const group1Hours = ['00:40', '03:40', '06:40', '09:40', '12:40', '15:40', '18:40', '21:40'];
-const group2Hours = ['01:40', '04:40', '07:40', '10:40', '13:40', '16:40', '19:40', '22:40'];
-const group3Hours = ['02:40', '05:40', '08:40', '11:40', '14:40', '17:40', '20:40', '23:40'];
-// const group4Hours = ['06:30', '10:30', '14:30', '18:30', '22:30', '02:30'];
+const group1Hours = ['01:45', '04:45', '07:45', '10:45', '13:45', '16:45', '19:45', '22:45'];
+const group2Hours = ['02:15', '05:15', '08:15', '11:15', '14:15', '17:15', '20:15', '23:15'];
+const group3Hours = ['02:45', '05:45', '08:45', '11:45', '14:45', '17:45', '20:45', '23:45'];
+const group4Hours = ['03:15', '06:15', '09:15', '12:15', '15:15', '18:15', '21:15', '00:15'];
+const group5Hours = ['03:45', '06:45', '09:45', '12:45', '15:45', '18:45', '21:45', '00:45'];
+const group6Hours = ['04:15', '07:15', '10:15', '13:15', '16:15', '19:15', '22:15', '01:15'];
 
 const messages = [
 	'Lets get those LE',
 	'Farming time 🌼',
 	'Another hour of hard watering! 💦',
 	'It is time to farm!',
-	'Next group in 1 hour',
 	'Long wait? 😁',
 ];
 
@@ -39,9 +40,19 @@ module.exports = {
 				`<@&${'879774522801291264'}> turn. ${messages[Math.floor(Math.random() * messages.length)]}`
 			);
 
-		// if (group4Hours.includes(date))
-		// 	return await channel.send(
-		// 		`<@&${'879979445149130782'}> turn. ${messages[Math.floor(Math.random() * messages.length)]}`
-		// 	);
+		if (group4Hours.includes(date))
+			return await channel.send(
+				`<@&${'879979445149130782'}> turn. ${messages[Math.floor(Math.random() * messages.length)]}`
+			);
+
+		if (group5Hours.includes(date))
+			return await channel.send(
+				`<@&${'881544844525305876'}> turn. ${messages[Math.floor(Math.random() * messages.length)]}`
+			);
+
+		if (group6Hours.includes(date))
+			return await channel.send(
+				`<@&${'881544926591086632'}> turn. ${messages[Math.floor(Math.random() * messages.length)]}`
+			);
 	},
 };
